74b970493f3996cab390ac3345ef0496d04fe4eb
erdemkarakas <erdemkarakas26@gmail.com>
2022-06-19 16:20:23 +0300

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/flowbite/**/*.js",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Open Sans"]
+      },
+      gridTemplateColumns: {
+        "1/5": "1fr 5fr"
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require('flowbite/plugin')],
+}
